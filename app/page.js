2464c15c48060543263c52313aa81124d45f10be
akshwalia <akshwalia20@gmail.com>
2024-01-30 22:40:16 +0530

    Minor changes

--- app/page.js
+++ app/page.js
@@ -1,34 +1,16 @@
 'use client';
+
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
-import { useState, useEffect } from 'react';
-
 
 const Page = () => {
-  // const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  // useEffect(() => {
-  //   const handleMouseMove = (e) => {
-  //     console.log(e.clientX, e.clientY);
-  //     setMousePosition({ x: e.pageX, y: e.pageY })
-  //   };
-
-  //   window.addEventListener('mousemove', handleMouseMove);
-
-  //   return () => {
-  //     window.removeEventListener('mousemove', handleMouseMove);
-  //   }
-  // },[]);
-
   const [showMenu, setShowMenu] = useState(false);
 
   return (
     <div className='bg-primary-black overflow-hidden'>
-      {/* Cursor */}
-      {/* <div className="cursor w-[20px] h-[20px] bg-white rounded-full z-20" 
-      style={{position: 'absolute', left: mousePosition.x-10, top: mousePosition.y-10}}
-       /> */}
+      
       <Navbar showMenu={showMenu} setShowMenu={setShowMenu} />
       <Hero />
       <div className="relative">
